8385b0209d5ed26ac6b82dc8b1a98ecffcc7c032
Kephas7 <diyalisimon7@gmail.com>
2025-01-13 11:03:16 +0545

    Logic to get all applications

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -33,3 +33,25 @@ exports.getAllUsers = async (req, res) => {
         });
     }
 };
+
+// Get all applications
+exports.getAllApp = async (req, res) => {
+    try {
+        const applications = await Application.findAll({
+            include: [
+                { model: Job, as: 'job' }, // Sequelize equivalent of population
+                { model: User, as: 'applicant' }
+            ]
+        });
+
+        res.status(200).json({
+            success: true,
+            applications
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
