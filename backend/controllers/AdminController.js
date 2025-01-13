8f93556cedafd042ea39d07651f3e9aa5f6fe563
Kephas7 <diyalisimon7@gmail.com>
2025-01-13 11:10:18 +0545

    Get User

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -188,3 +188,26 @@ exports.deleteUser = async (req, res) => {
     }
 };
 
+// Get User
+exports.getUser = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id); // Sequelize equivalent of find by ID
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            user
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
